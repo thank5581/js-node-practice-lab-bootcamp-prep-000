7aa4764c9be4442c6a8db31c0d851332bbdd97ff
thank5581 <thank5581@gmail.com>
2020-06-17 13:45:38 -0400

    node

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@
 
 var name = "Joe"
 var height = "74"
+var name = "Thanh"
+var height = "163"
 
 console.log("Name:")
 console.log(name)
+console.log("Name:");
+console.log(name);
 
 console.log("Height:")
 console.log(height)
+console.log("Height:");
+console.log(parseInt(height)+1);
